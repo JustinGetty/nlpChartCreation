ae9b268d5e3868447db8730413140c8763a0a0b3
JustinGetty <jrg349@drexel.edu>
2024-03-22 12:56:52 -0400

    working function, note add 1 second to end of endDate to prevent inclusive/exclusive problem

--- brandtBot.py
+++ brandtBot.py
@@ -9,37 +9,43 @@ import mplfinance as mpf
 
 #index=pd.date_range("2021-11-10", periods=7, freq="d")) 
 #Important keys
-startDate = "03/18/2024 09:30:00"
-endDate = "03/18/2024 15:59:00"
 
-aapl = get_data("aapl", start_date=startDate, end_date = "03/18/2024 19:59:00", interval="1m")
-dateRange = pd.date_range(startDate, endDate, freq='T')
-dates = []
-for date in dateRange:
-    dates.append(date)
-print(f"Length: {len(dates)}")
+def createChart(company, startDate, endDate, interval='1d'):
+    #This is UTC
+    stock = get_data(company, start_date=startDate, end_date = endDate, interval=interval)
+    #freq, T for min, H, D, M, Y
+    interval = interval.replace('m', 'T').upper()
+    #This is EST
+    print(f"Interval: {interval}")
+    dateRange = pd.date_range(startDate, endDate, freq=interval)
+    dates = []
+    for date in dateRange:
+        dates.append(date)
+    print(f"Length: {len(dates)}")
 
-print(aapl)
-aapl.insert(0, 'date', dates) 
-print(aapl)
-print(aapl.keys())
+    print(stock)
+    stock.insert(0, 'date', dates) 
+    print(stock)
+    print(stock.keys())
 
-ohlc = aapl.loc[:, ['open', 'high', 'low', 'close']]
-ohlc.set_index(dateRange)
-mpf.plot(ohlc, type='candle', style='charles')
+    ohlc = stock.loc[:, ['open', 'high', 'low', 'close']]
+    ohlc.set_index(dateRange)
+    mpf.plot(ohlc, type='candle', style='charles')
 
-print(ohlc)
+    print(ohlc)
 
-plt.figure(figsize=(17, 10))
-fig, ax = plt.subplots()
+    plt.figure(figsize=(17, 10))
+    fig, ax = plt.subplots()
+
+    #plt.plot(dates, stock['high'])
+    fig.tight_layout()
+    plt.grid(True)
 
 
-'''
-plt.plot(dates, aapl['high'])
-plt.plot(dates, aapl['low'])
-plt.plot(dates, aapl['open'])
-plt.plot(dates, aapl['close'])
-'''
-fig.tight_layout()
-plt.grid(True)
+if __name__ == '__main__':
+    
+    #use UTC because yahoo finance is cringe
+    startDate = "03/18/2024 13:30:00"
+    endDate = "03/18/2024 19:59:01"
 
+    createChart('aapl', startDate, endDate, interval='1m')
